98664c3c835b0aaa2c4feaa4c248b0d5130ee52f
jakub.pawlowski <pawlowskijakub@gmail.com>
2017-08-18 08:42:49 +0200

    Shorten workspace on master node too. Add a switch to force plugin even if worspace seems short enough.

--- src/main/java/org/jenkinsci/plugins/shortwspath/ShortWsLocator.java
+++ src/main/java/org/jenkinsci/plugins/shortwspath/ShortWsLocator.java
@@ -34,11 +34,12 @@ import hudson.remoting.VirtualChannel;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import jenkins.model.Jenkins;
 import jenkins.slaves.WorkspaceLocator;
 
 import org.apache.commons.lang.StringUtils;
@@ -55,50 +56,104 @@ public class ShortWsLocator extends WorkspaceLocator {
      * The number is not supposed to include the path to the workspace itself.
      */
     private static int BUILD_PATH_LENGTH = Integer.getInteger("org.jenkinsci.plugins.shortwspath.BUILD_PATH_LENGTH", 512);
+    private static boolean FORCE_SHORT_WS = true;
+    private static boolean FORCE_MASTER = true;
 
     // To be invalidated when slave is reconnected
     private final Map<Node, Integer> cachedMaxLengths = new WeakHashMap<Node, Integer>();
 
     @Override
     public FilePath locate(TopLevelItem item, Node node) {
-        if (!(node instanceof Slave)) {
-            // noop on master. The path is configurable and rather tricky to replace safely
+        LOGGER.info(String.format("locate item %s at node %s. force_master=%s force_short_ws=%s", item, node, FORCE_MASTER, FORCE_SHORT_WS));
+
+        if (!(node instanceof Slave) && !FORCE_MASTER) {
+            LOGGER.info("ignoring master node");
             return null;
         }
-        Slave slave = (Slave) node;
-
-        FilePath def = getDefaultPath(item, slave);
-        if (def == null) return null; // No idea what the path is going to look like - do not touch
-
-        int usabeSpace = getUsableSpace(def, node);
-        if (usabeSpace > BUILD_PATH_LENGTH) return null; // There is plenty of room
 
         String itemName = StringUtils.abbreviate(item.getName(), 0, 16);
+        
+        FilePath def = getDefaultPath(item, node);
+        if (def == null) {
+            LOGGER.info("default path == null. No idea what the path is going to look like - do not touch");
+            return null; // No idea what the path is going to look like - do not touch
+        }
+        LOGGER.info(String.format("default path == %s", def));
+
+        int usabeSpace = getUsableSpace(def, node);
+        if (usabeSpace > BUILD_PATH_LENGTH && !FORCE_SHORT_WS) { 
+            LOGGER.info(String.format("usable space (%d) > BUILD_PATH_LENGTH (%d)", usabeSpace, BUILD_PATH_LENGTH));
+            return null; // There is plenty of room
+        }
+
+        
         // Replace the ellipsis with dashes to avoid problems with msbuild
         // prior to version 4.6.2.  It used its own path normalization (vs. built in .NET)
         // which doesn't recognize ... as a valid path.
-        itemName = itemName.replace("...", "---");
+        itemName = itemName.replace("...", "_");
         final String digest = Util.getDigestOf(item.getFullName()).substring(0, 8);
-        FilePath workspaceRoot = slave.getWorkspaceRoot();
-        if (workspaceRoot == null) return null; // Slave went offline
+        FilePath workspaceRoot = getWorkspaceRoot(item, node);
+        if (workspaceRoot == null) {
+            LOGGER.info("no workspace root found");
+            return null; // Slave went offline
+        }
         FilePath newPath = workspaceRoot.child(itemName + digest);
 
-        return newPath.getRemote().length() < def.getRemote().length()
-                ? newPath
-                : null // Do nothing if it will not improve the situation
-        ;
+        if (newPath.getRemote().length() < def.getRemote().length() || FORCE_SHORT_WS) {
+            LOGGER.info(String.format("returning shortened path: %s", newPath));
+            return newPath;
+        } else {
+            LOGGER.info(String.format("shortened path: %s >= original path: %s", newPath, def));
+            return null; // Do nothing if it will not improve the situation
+        }
+        
     }
 
     /**
      * Mimic core behaviour in deducing the path length.
      */
+     
+	private FilePath getWorkspaceRoot(TopLevelItem item, Node node) {
+        if ((node instanceof Slave)) { 
+            return getWorkspaceRoot(item, (Slave)node);
+        } 
+        else if ((node instanceof Jenkins)) { 
+            FilePath rootPath = ((Jenkins)node).getRootPath();
+            String ws = ((Jenkins)node).getRawWorkspaceDir();
+            String newWs = "workspace\\" + item.getFullName().replace('/','_');
+            LOGGER.info(String.format("rootpath=%s ws=%s",rootPath, newWs));
+            return new FilePath(rootPath, newWs);
+        }
+        else {
+            return node.getWorkspaceFor(item);
+        }
+	}
+
+    private FilePath getWorkspaceRoot(TopLevelItem item, Slave slave) {
+		return slave.getWorkspaceRoot();
+	}
+     
     private FilePath getDefaultPath(TopLevelItem item, Slave slave) {
+        
         final String itemFullName = item.getFullName();
 
-        FilePath wsroot = slave.getWorkspaceRoot();
+        FilePath wsroot = getWorkspaceRoot(item, slave);
         if (wsroot == null) return null; // Offline
 
         return wsroot.child(itemFullName);
+    }
+    
+    private FilePath getDefaultPath(TopLevelItem item, Node node) {
+        if ((node instanceof Slave)) { 
+            return getDefaultPath(item, (Slave)node);
+        } 
+        else {
+            final String itemFullName = item.getFullName();
+
+            FilePath wsroot = getWorkspaceRoot(item, node);
+            if (wsroot == null) return null; // Offline
+            return wsroot.child(itemFullName);
+        }
     }
 
     private int getUsableSpace(FilePath path, Node node) {
@@ -116,8 +171,8 @@ public class ShortWsLocator extends WorkspaceLocator {
                 return Integer.MAX_VALUE; // Do not intercept
             }
         }
-
         int prefixLength = path.getRemote().length();
+        LOGGER.info(String.format("usable space=max(%d)-%d (%s)", platformMax, prefixLength, path.getRemote()));
         return platformMax - prefixLength;
     }
 
